91bfb536a09bf86d0059b3ced3946f7d7d407425
Siddharth Bhardwaj <25156849+SidB16@users.noreply.github.com>
2019-12-17 07:00:21 -0500

    Delete dff_async_clear.v

--- dff_async_clear.v
+++
@@ -1,8 +0,0 @@
-module mydffpos(input d, clk, clr, output qbar, output reg q);
-	assign qbar = ~q;
-	always@(posedge clk, negedge clr)//async clear
-	begin
-		if(!clr) q<=0;
-		else q<= d;
-	end
-endmodule
